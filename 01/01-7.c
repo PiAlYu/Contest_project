b62de6a0c832d69ca5e2b121b295862d2d69a6ff
PiAlYu <157111194+PiAlYu@users.noreply.github.com>
2024-10-11 22:25:52 +0300

    Create 01-7.c

---
+++ 01/01-7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    unsigned int n = 0;
+    int k = 0;
+    scanf("%u", &n);
+    scanf("%d", &k);
+    unsigned int a = n & ((1 << k) - 1);
+    printf("%u", a);
+}
