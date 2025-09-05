25904b0a09c71c58c4420eabe808b0770eea9e93
Alexander Pirogov <157111194+PiAlYu@users.noreply.github.com>
2025-09-05 11:40:25 +0300

    Update mz-04.c

--- 2 kurs/mz-0/mz-04.c
+++ 2 kurs/mz-0/mz-04.c
@@ -2,43 +2,24 @@
 #include <stdlib.h>
 
 int
-compare_value_ch(const void *a, const void *b)
+compare(const void *a, const void *b)
 {
     int arg1 = *(const int *) a;
     int arg2 = *(const int *) b;
-    return (arg1 > arg2) - (arg1 < arg2);
-}
 
-int
-compare_value_nech(const void *a, const void *b)
-{
-    int arg1 = *(const int *) a;
-    int arg2 = *(const int *) b;
-    return (arg1 < arg2) - (arg1 > arg2);
-}
+    if ((arg1 % 2 == 0) != (arg2 % 2 == 0)) {
+        return (arg2 % 2 == 0) - (arg1 % 2 == 0);
+    }
 
-int
-compare_ost(const void *a, const void *b)
-{
-    int arg1 = *(const int *) a;
-    int arg2 = *(const int *) b;
-    return (arg2 % 2 == 0) - (arg1 % 2 == 0);
+    if (arg1 % 2 == 0) {
+        return (arg1 > arg2) - (arg1 < arg2);
+    } else {
+        return (arg1 < arg2) - (arg1 > arg2);
+    }
 }
 
 void
 sort_even_odd(size_t count, int *data)
 {
-    qsort(data, count, sizeof(int), compare_ost);
-
-    size_t partition_index = count;
-    for (int i = 0; i < count; i++) {
-        if (data[i] % 2 != 0) {
-            partition_index = i;
-            break;
-        }
-    }
-
-    qsort(data, partition_index, sizeof(int), compare_value_ch);
-
-    qsort(data + partition_index, count - partition_index, sizeof(int), compare_value_nech);
+    qsort(data, count, sizeof(int), compare);
 }
