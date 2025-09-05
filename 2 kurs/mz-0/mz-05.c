920c8cfbd106888b4e0c8b1bebb4f4b2267aa86e
Alexander Pirogov <157111194+PiAlYu@users.noreply.github.com>
2025-09-05 11:41:21 +0300

    Update mz-05.c

--- 2 kurs/mz-0/mz-05.c
+++ 2 kurs/mz-0/mz-05.c
@@ -2,31 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-void recursion(char *str, char *alp, int l, int n)
+void
+recursion(char *str, char *alp, int l, int n)
 {
-    if (l == n) printf("%s\n", str); // вывод ответа
-    else {
-        char *strc = malloc(sizeof(str));   // создание копии
-        strcpy(strc, str);
-        strc[n] = '\0';
+    if (l == n) {
+        str[l] = '\0';
+        printf("%s\n", str);
+    } else {
         for (int i = 0; i < n; i++) {
-            if (alp[i] != '0') {
-                strc[l++] = alp[i];         // добавление символа
-                alp[i] = '0';               // исключение из алфавита
-                recursion(strc, alp, l, n); // вызов рекурсии
-                alp[i] = '1' + i;           // возврат к алфавиту
-                l--;
+            if (alp[i] == 0) {
+                alp[i] = 1;
+                str[l] = '1' + i;
+                recursion(str, alp, l + 1, n);
+                alp[i] = 0;
             }
         }
     }
 }
 
-int main(void)
+int
+main(void)
 {
     int n;
-    scanf("%d", &n);                // считывание n
-    char alp[n], str[n + 1];
-    for (int i = 0; i < n; i++) alp[i] = '1' + i; // создание алфавита
-    recursion(str, alp, 0, n);           // вызов рекурсии
+    scanf("%d", &n);
+    char str[n + 1], alp[n];
+    for (int i = 0; i < n; i++) {
+        alp[i] = 0;
+    }
+    recursion(str, alp, 0, n);
     return 0;
 }
