f22d760a062bc137771b176f1248f45a21aef985
Alexander Pirogov <157111194+PiAlYu@users.noreply.github.com>
2025-09-15 17:36:55 +0300

    Update mz-03.c

--- 2 kurs/mz-0/mz-03.c
+++ 2 kurs/mz-0/mz-03.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long long pow_(long long a, long long b, long long mod) {
-    long long x = 1;
-    while (b > 0) {
-        if (b % 2 == 1){
-            x = (x * a) % mod;
+enum
+{
+    N_MAX = 2000,
+    N_MIN = 2
+};
+
+int
+mod_pow(int base, int power, int mod)
+{
+    int res = 1;
+    while (power != 0) {
+        if (power % 2 == 1) {
+            res = (res * base) % mod;
         }
-        b = b / 2;
-        a = (a * a) % mod;
+        base = (base * base) % mod;
+        power >>= 1;
     }
-    return x;
+    return res;
 }
 
-int main(void) {
-    long long n;
-    scanf("%lld", &n);
-
+int
+main(void)
+{
+    int n;
+    if (scanf("%d", &n) < 1) {
+        return 1;
+    } else if (n >= N_MAX || n < N_MIN) {
+        return 1;
+    }
     for (int i = 0; i < n; i++) {
-        for (int a = 1; a < n; a++) {
-            long long  ans = ((i * pow_(a, n - 2, n)) % n);
-            printf("%lld ", ans);
+        for (int j = 1; j < n; j++) {
+            printf("%d ", (mod_pow(j, n - 2, n) * i) % n);
         }
         printf("\n");
     }
